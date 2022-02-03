80eb62e30cbcb23b617016421b2f5003c6c9772a
unknown <chaperomauro1803@gmail.com>
2022-02-02 22:41:19 -0300

    Primer commit, se crearon los archivos Main, Pais, ServicioPais

--- src/coleccionesejercicio005/ColeccionesEjercicio005.java
+++ src/coleccionesejercicio005/ColeccionesEjercicio005.java
@@ -1,10 +1,12 @@
 package coleccionesejercicio005;
 
+import java.util.Scanner;
+
 
 public class ColeccionesEjercicio005 {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
     }
 
 }
--- src/coleccionesejercicio005/servicios/ServicioPais.java
+++ src/coleccionesejercicio005/servicios/ServicioPais.java
@@ -1,6 +1,8 @@
 package coleccionesejercicio005.servicios;
 
+import java.util.Scanner;
+
 
 public class ServicioPais {
-
+    Scanner sc = new Scanner(System.in).useDelimiter("\n");
 }
